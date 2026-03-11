5b7d2804fe8a5895309f23fee052cfb7d5554ed1
Shafa-Sabir <shafarana801@gmail.com>
2026-03-11 12:14:28 +0500

    pass by reference

--- class.cpp
+++ class.cpp
@@ -1,34 +1,11 @@
 #include<iostream>
 using namespace std;
-class Student{
-    public:
-    Student(int a,string Std_name,int Std_roll_no)
-    {
-       age=a;
-       name=Std_name;
-       roll_no=Std_roll_no;
 
 
-    }
-    private:
-    int age;
-    string name;
-    int roll_no;
-    public:
-    void displays(){
-        cout<<age<<endl;
-        cout<<name<<endl;
-        cout<<roll_no<<endl;
-
-    }
-    
-
-
-};
 int main(){
-    Student S1(45,"Shafa",2347765);
-   S1. displays();
-    
+    int org_var=3;
+    int &alias_name=org_var;
+    cout<<"The value of variable"<<org_var<<endl;
+
+
 }
-
-
