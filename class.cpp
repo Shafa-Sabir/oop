f095ed8e4f4bac10464c5d28da6efcc10113a928
Shafa-Sabir <shafarana801@gmail.com>
2026-03-11 12:21:27 +0500

    pass by reference

--- class.cpp
+++ class.cpp
@@ -5,7 +5,13 @@ using namespace std;
 int main(){
     int org_var=3;
     int &alias_name=org_var;
+    cout<<alias_name<<endl;
     cout<<"The value of variable"<<org_var<<endl;
+    cout<<&alias_name<<endl;
+    cout<<&org_var<<endl;
+    alias_name=6;
+    cout<<org_var<<endl;
+    return 0;
 
 
 }
