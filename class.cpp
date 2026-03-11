a1ad04422e8e1f0eee86a578bbe4412a4b905064
Shafa-Sabir <shafarana801@gmail.com>
2026-03-11 14:41:02 +0500

    copy constructor

--- class.cpp
+++ class.cpp
@@ -1,17 +1,39 @@
 #include<iostream>
 using namespace std;
 
+class Student{
+    private:
+    int roll_no;
+    string name;
+    string Dep;
+    public:
+    Student(int roll_no, string name,string Dep){
+        this->roll_no=roll_no;
+        this->name=name;
+        this->Dep=Dep;
 
-int main(){
-    int org_var=3;
-    int &alias_name=org_var;
-    cout<<alias_name<<endl;
-    cout<<"The value of variable"<<org_var<<endl;
-    cout<<&alias_name<<endl;
-    cout<<&org_var<<endl;
-    alias_name=6;
-    cout<<org_var<<endl;
-    return 0;
+
+    }
+    Student(Student &s3){
+        this->roll_no=s3.roll_no;
+        this->name=s3.name;
+        this->Dep=s3.Dep;
+
+    }
+   void display_information(){
+    cout<<roll_no<<name<<Dep<<endl;
+   }
+
+};
+int main (){
+    Student s1(13,"shafa","computer science");
+    Student s2(s1);
+    s1.display_information();
+    s2.display_information();
+
 
 
 }
+
+
+
